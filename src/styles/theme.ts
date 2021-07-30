28e368f873db42972945bf5730077d41494b1a8d
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-30 16:27:30 -0300

    feat: :sparkles: mediaQuery theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,20 +1,23 @@
 export const theme = {
   colors: {
-    black: '#000000',
+    black: "#000000",
     text: {
-      primary: '#FFFFFF',
+      primary: "#FFFFFF",
     },
   },
   fontSize: {
-    small: '0.5rem',
-    normal: '1rem',
-    large: '2rem',
-    huge: '7rem',
+    small: "0.5rem",
+    normal: "1rem",
+    large: "2rem",
+    huge: "7rem",
   },
   fontWeight: {
     light: 300,
     regular: 400,
     bold: 500,
+  },
+  mediaQuery: {
+    "915": "915px",
   },
 };
 
