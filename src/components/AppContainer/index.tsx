c926d07bc67d8b80f8d3bd216fba4534a720d75f
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-30 12:59:19 -0300

    fix: :ambulance: importing head from next/head

--- src/components/AppContainer/index.tsx
+++ src/components/AppContainer/index.tsx
@@ -1,22 +1,7 @@
-import { useEffect, useState } from 'react';
 import * as AppContainerStyles from './styles';
 
 export const AppContainer: React.FC = ({ children }) => {
-  const [userPosition, setUserPosition] = useState<any>();
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log(position);
-      setUserPosition(position);
-    });
-  }, []);
-
-  console.log('userPosition', userPosition);
-
   return (
-    <AppContainerStyles.Container>
-      {userPosition}
-      {children}
-    </AppContainerStyles.Container>
+    <AppContainerStyles.Container>{children}</AppContainerStyles.Container>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { Head } from 'next/document';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../styles/global';
+import { theme } from '../styles/theme';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
-        rel="stylesheet"
-      />
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>next.Weather</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalStyles />
       <Component {...pageProps} />
-    </Head>
+    </ThemeProvider>
   );
 }
 
