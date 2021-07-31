77c3167d46eb68ebca4089cb682ae605c229088b
Felipe <felipe.araujo@hubfintech.com.br>
2021-07-31 13:52:21 -0300

    feat: :sparkles: filling image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,27 @@ import { Container } from '../components/AppContainer/styles';
 import { WeatherInfo } from '../components/WeatherInfo';
 import { WeatherRegion } from '../components/WeatherRegion';
 
+import { isMobile } from 'react-device-detect';
+
+import { useCallback, useEffect, useState } from 'react';
+import { getFromLocalStorage } from '../utils/localStorage';
+
 export default function Home() {
+  const fillImage: any = useCallback(async () => {
+    const photo = await getFromLocalStorage('imagem', isMobile);
+    return setImage(photo);
+  }, []);
+
+  const [image, setImage] = useState(fillImage);
+
+  useEffect(() => {
+    if (image === null) {
+      fillImage();
+    }
+  }, [image]);
+
   return (
-    <Container>
+    <Container image={image}>
       <WeatherRegion />
       <WeatherInfo />
     </Container>
