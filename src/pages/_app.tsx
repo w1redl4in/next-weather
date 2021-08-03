cc26ddbd834f3caba1b59858f554572a20ce5e04
Felipe <felipe.araujo@hubfintech.com.br>
2021-08-03 16:53:36 -0300

    refactor: :recycle: using context providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,28 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import { ImageProvider } from '../contexts/image';
+import { WeatherProvider } from '../contexts/weather';
 import { GlobalStyles } from '../styles/global';
 import { theme } from '../styles/theme';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <title>next.weather</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <ImageProvider>
+        <WeatherProvider>
+          <Head>
+            <title>next.weather</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </WeatherProvider>
+      </ImageProvider>
     </ThemeProvider>
   );
 }
